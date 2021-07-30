8ded0d6f3681d91313db0b11c7e4e0d2c36d232d
jungseungtae <jstcom3@naver.com>
2021-07-30 19:50:09 +0900

    짜왕

--- day_21.07.30.py
+++ day_21.07.30.py
@@ -27,5 +27,34 @@
 #   print(i, end = ' ')
 # print()
 
-### 3. 
+### 3. 2자리 양수(10~99) 입력받기
+
+# print('2자리 양수 입력')
+#
+# while True:
+#   no = int(input('값 입력 : '))
+#   # if 10 <= no <= 99:
+#   if not (no < 10 or no > 99):  #드로므간의 법칙
+#     break
+# print(f'입력받은 양수는 {no}입니다.')
+
+### 4. 구구단
+
+# print('-' * 27)
+# for i in range(2, 10):    # 행
+#   for j in range(1, 10):  # 열
+#     print(f'{i * j:3}', end = '') # 자리수를 3자리로
+#   print()
+# print('-' * 27)
+
+### 5. * 직각 삼각형
+
+n=int(input('짧은 변의 길이 입력 : '))
+
+for i in range(n):
+  for j in range(i+1):
+    print('*', end = '')
+  print()
+
+### 5-1 반대로 출력
 
