6f37658c549e2b12781386c4f0d42ebba6f41ca7
jungseungtae <jstcom3@naver.com>
2022-01-23 17:23:46 +0900

    짜왕

--- Drinks.py
+++ Drinks.py
@@ -1,11 +1,10 @@
 ###########################
 # 전세계 음주 데이터 분석하기 #
 ###########################
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
 
 file_path = "C:/Users/jstco/Downloads/drinks.csv"
 drinks = pd.read_csv(file_path)
@@ -54,10 +53,67 @@ drinks['continent'] = drinks['continent'].fillna('OT')
 # print(drinks.head(10))
 
 # 파이차트
+
+# 대륙의 이름을 저장한다.
 labels = drinks['continent'].value_counts().index.tolist()
+# 대륙 안의 국가의 수를 저장한다.
 fracs1 = drinks['continent'].value_counts().values.tolist()
+# 원 그래프의 조각 분리 간격을 설정한다.
 explode = (0, 0, 0, 0.1, 0, 0)
 
-plt.pie(fracs1, explode = explode, labels = labels, autopct = '%0.f%%')
-plt.title('Null data to \'OT\'')
+# plt.pie(fracs1, explode = explode, labels = labels, autopct = '%0.f%%')
+# plt.title('Null data to \'OT\'')
+# plt.show()
+
+# 그룹단위 데이터 분석
+result = drinks.groupby('continent').spirit_servings.agg(['mean', 'min', 'max', 'sum'])
+# print(result)
+
+# 전체 평균 이상 알코올 섭취하는 대륙
+total_mean = drinks.total_litres_of_pure_alcohol.mean()
+continent_mean = drinks.groupby('continent')['total_litres_of_pure_alcohol'].mean()
+over_continent = continent_mean[continent_mean >= total_mean]
+# print(over_continent)
+# print(continent_mean)
+
+# 맥주 소비가 가장 많은 대륙
+beer_top = drinks.groupby('continent').beer_servings.mean().idxmax()
+# print(beer_top)
+
+# 분석결과 시각화
+# n_groups = len(result.index)
+# means = result['mean'].tolist()
+# mins = result['min'].tolist()
+# maxs = result['max'].tolist()
+# sums = result['sum'].tolist()\
+#
+# index = np.arange(n_groups)
+# bar_width = 0.1
+#
+# rects1 = plt.bar(index, means, bar_width, color = 'r', label = 'Mean')
+# rects2 = plt.bar(index + bar_width, mins, bar_width, color = 'g', label = 'Min')
+# rects3 = plt.bar(index + bar_width * 2, maxs, bar_width, color = 'b', label = 'Max')
+# rects3 = plt.bar(index + bar_width * 3, sums, bar_width, color = 'y', label = 'Sum')
+
+# plt.xticks(index, result.index.tolist())
+# plt.legend()
+# plt.show()
+
+# 대륙별 퓨어 알콜
+continents = continent_mean.index.tolist()
+continents.append('mean')
+
+x_pos = np.arange(len(continents))
+alcohol = continent_mean.tolist()
+alcohol.append(total_mean)
+
+bar_list = plt.bar(x_pos, alcohol, align = 'center', alpha = 0.5)
+bar_list[len(continents) - 1].set_color('r')
+plt.plot([0, 6], [total_mean, total_mean], 'k--')
+plt.xticks(x_pos, continents)
+
+plt.ylabel('Total litres of Pure alcohol')
+plt.title('Total litres of Pure alcohol by Continent')
+
 plt.show()
+
