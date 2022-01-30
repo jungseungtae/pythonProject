a127ff6970d9ac2a6535e29beb48a71564ad35b3
jungseungtae <jstcom3@naver.com>
2022-01-30 20:52:30 +0900

    짜왕

--- PuzzleAlgorithms.py
+++ PuzzleAlgorithms.py
@@ -1,4 +1,7 @@
+########################
 ## 1. 모두 똑같이 만들기 ##
+########################
+# 바꾸어야 할 배열의 순서 찾기
 
 cap1 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'B', 'F']
 cap2 = ['F', 'F', 'B', 'B', 'B', 'F', 'B', 'B', 'B', 'F', 'F', 'F', 'F']
@@ -35,7 +38,7 @@ def pleaseConform(caps):
         print('People in positions', t[0], 'through', t[1], 'flip your caps!')
     print(t[0:])
 
-pleaseConform(cap1)
+# pleaseConform(cap1)
 
 # 리스트에 + [] 배열로 추가하는 방법은 기존 리스트에 변화를 주지 않는다.
 def listConcatenate(caps):
@@ -115,5 +118,88 @@ def solution(s):
   return print(result)
 
 # s = 'ababcdcdababcdcd'
-s = 'abcabcdede'
+# s = 'abcabcdede'
 # solution(s)
+
+##################################
+## 2. 파티에 참석하기 가장 좋은 시간 ##
+##################################
+# 가장 중복이 많은 시간 찾기
+
+sched =  [(6, 8), (6, 12), (6, 7), (7, 8),
+          (7, 10), (8, 9), (8, 10), (9, 12),
+          (9, 10), (10, 11), (10, 12), (11, 12)]
+
+sched2 = [(6.0, 8.0), (6.5, 12.0), (6.5, 7.0), (7.0, 8.0), (7.5, 10.0), (8.0, 9.0),
+          (8.0, 10.0), (9.0, 12.0), (9.5, 10.0), (10.0, 11.0), (10.0, 12.0), (11.0, 12.0)]
+
+def bestTimeToParty(schedule):
+  start = schedule[0][0]
+  end = schedule[0][1]
+  for c in schedule:
+    start = min(c[0], start)
+    end = max(c[1], end)
+  count = celebrityDensity(schedule, start, end)
+  # print(start, end, c[0:], count)
+  # maxcount = 0
+  # for i in range(start, end + 1):
+  #   if count[i] > maxcount:
+  #     maxcount = count[i]
+  #     time = i
+  # 위 주석부분 변경가능
+  maxcount = max(count[start:end + 1])
+  time = count.index(maxcount)
+  # print(maxcount, time)
+  print('Best time to attend the party is at',
+    time, 'o\'clock', ':', maxcount, 'celebrities will be attending!')
+
+def celebrityDensity(sched, start, end):
+  count = [0] * (end + 1)
+  for i in range(start, end + 1):
+    count[i] = 0
+    for c in sched:
+      if c[0] <= i and c[1] > i:
+        count[i] += 1
+        # print(count[i], end = ' ')
+        # print(count)
+  return count
+
+bestTimeToParty(sched)
+
+def bestTimeToPartySmart(schedule):
+  times = []
+  for c in schedule:
+    times.append((c[0], 'start'))
+    times.append((c[1], 'end'))
+  sortList(times)
+  maxcount, time = chooseTime(times)
+  print('Best time to attend the party is at',
+    time, 'o\'clock', ':', maxcount, 'celebrities will be attending!')
+
+def sortList(tlist):
+  for ind in range(len(tlist)-1):
+    iSm = ind
+    for i in range(ind, len(tlist)):
+      if tlist[iSm][0] > tlist[i][0]:
+        iSm = i
+    tlist[ind], tlist[iSm] = tlist[iSm], tlist[ind]
+  return
+
+def chooseTime(times):
+
+  rcount = 0
+  maxcount = 0
+  time = 0
+
+  for t in times:
+    if t[1] == 'start':
+      rcount = rcount + 1
+    elif t[1] == 'end':
+      rcount = rcount - 1
+    if rcount > maxcount:
+      maxcount = rcount
+      time = t[0]
+
+  return maxcount, time
+
+bestTimeToPartySmart(sched2)
